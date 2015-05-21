47b3c8bb46efa90ce25ecf80cf70470b3e6b931d
theB264 <public88878878887@hotmail.com>
2015-05-21 14:25:51 +0000

    refactor nil

--- lib/factory-helper/string.rb
+++ lib/factory-helper/string.rb
@@ -4,14 +4,14 @@ module FactoryHelper
       if [Range, Array].include? length.class
         utf8string length.to_a.sample
       else
-        utf8string (length|| 0)
+        utf8string length
       end
     end
 
   protected
 
     def self.utf8string(length)
-      (1..length).map do |_|
+      (1..length.to_i).map do |_|
         [rand(58)+ 32, rand(127), rand(0xd800), rand(0x102000)+ 0xe000].
           sample.chr(Encoding::UTF_8)
       end.join.force_encoding("UTF-8")
