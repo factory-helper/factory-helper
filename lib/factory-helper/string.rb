e38f30a2c5535fa2c33c2a339efd8c81fefa519c
theB264 <public88878878887@hotmail.com>
2015-05-21 12:52:47 +0000

    add missing utf-8 codepoint

--- lib/factory-helper/string.rb
+++ lib/factory-helper/string.rb
@@ -12,7 +12,7 @@ module FactoryHelper
 
     def self.utf8string(length)
       (1..length).map do |_|
-        [rand(58)+32,rand(127),rand(0xd800),rand(0x101fff)+0xe000].
+        [rand(58)+32,rand(127),rand(0xd800),rand(0x102000)+0xe000].
           sample.chr(Encoding::UTF_8)
       end.join.force_encoding("UTF-8")
     end
