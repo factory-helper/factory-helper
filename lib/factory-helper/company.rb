f484eccc5fe161d48c66a05f324353a0dd52b08f
Nikita Avvakumov <nikitaavvakumov@gmail.com>
2015-05-26 13:15:19 +0300

    Minor editing

--- lib/factory-helper/company.rb
+++ lib/factory-helper/company.rb
@@ -17,7 +17,7 @@ module FactoryHelper
       end
 
       def buzzword
-        translate('faker.company.buzzwords').flatten.sample
+        translate('factory_helper.company.buzzwords').flatten.sample
       end
 
       # When a straight answer won't do, BS to the rescue!
