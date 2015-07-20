faker-ruby/faker

fix domain_word to work correctly on all locales

lib/faker/internet.rb
@@ -72,7 +72,10 @@ module Faker
       end
 
       def domain_word
-        Company.name.split(' ').first.gsub(/\W/, '').downcase
+        Faker::Config.locale = nil
+        I18n.with_locale('en') do
+          return Company.name.split(' ').first.gsub(/\W/, '').downcase
+        end
       end
 
       def domain_suffix

factory-helper/factory-helper

lib/factory-helper/internet.rb
@@ -68,7 +68,10 @@ module FactoryHelper
       end
 
       def domain_word
-        Company.name.split(' ').first.gsub(/\W/, '').downcase
+        FactoryHelper::Config.locale = nil
+        I18n.with_locale('en') do
+          return Company.name.split(' ').first.gsub(/\W/, '').downcase
+        end
       end
 
       def domain_suffix