38366621dd1c251dfdcbae4d2df7d2070710904c
razor <razorcd@yahoo.com>
2015-07-24 17:46:34 +0300

    fixes seed= return error

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe FactoryHelper::Config do
   describe 'PRNG' do
     specify '.seed= returns new seed' do
-      expect(FactoryHelper::Config.seed= nil).to eq FactoryHelper::Config.seed
+      expect(FactoryHelper::Config.set_seed nil).to eq FactoryHelper::Config.seed
     end
 
     specify '.random.seed' do
@@ -10,13 +10,13 @@ RSpec.describe FactoryHelper::Config do
 
     specify 'use Random for entropy' do
       expect(Random).to receive(:new_seed)
-      FactoryHelper::Config.seed= nil
+      FactoryHelper::Config.set_seed nil
     end
 
     #use two entropy sources because "Starting with Unicorn 3.6.1, we have builtin workarounds for Kernel#rand and OpenSSL::Random users, but applications may use other PRNGs."
     specify 'use Kernel.rand for entropy' do
       expect(Kernel).to receive(:rand)
-      FactoryHelper::Config.seed= nil
+      FactoryHelper::Config.set_seed nil
     end
   end
 end
