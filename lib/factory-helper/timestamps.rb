c647d0497c325e63dc50ef8e3d0624d40f2c2fd7
razor <razorcd@yahoo.com>
2015-12-22 14:12:15 +0200

    adds test_forward_pseudorandom and test_backward_pseudorandom and rounds floating point operations

--- lib/factory-helper/timestamps.rb
+++ lib/factory-helper/timestamps.rb
@@ -7,18 +7,18 @@ module FactoryHelper
       end
 
       def forward(from = ::Time.at(0), seconds = 60)
-        to = from + seconds
+        to = (from + seconds).round(9)
         random_time(from, to)
       end
 
       def backward(from = ::Time.at(0), seconds = 60)
-        to = from - seconds
+        to = (from - seconds).round(9)
         random_time(to, from)
       end
 
     private
 
-      def random_time(from, to)
+      def random_time(from, to) # not inclusive
         diff_time = (to - from).round(9)
         diff_in_nsec = (diff_time * 1_000_000_000).to_i
         rand_in_nsec = FactoryHelper::Config.random.rand(diff_in_nsec-1) + 1 # avoids 0
--- test/test_factory_helper_timestamp.rb
+++ test/test_factory_helper_timestamp.rb
@@ -11,41 +11,61 @@ class TestFactoryHelperTimestamps < Test::Unit::TestCase
 
     100.times do
       random_time = @tester.between(from, to)
-      assert random_time.to_r > from.to_r, "Expected > \"#{strftime_nsec from}\", but got #{strftime_nsec random_time}"
-      assert random_time.to_r < to.to_r  , "Expected < \"#{strftime_nsec to}\", but got #{strftime_nsec random_time}"
+      assert random_time.to_r > from.to_r, "Expected > \"#{strftime_nsec from}\", but got \"#{strftime_nsec random_time}\""
+      assert random_time.to_r < to.to_r  , "Expected < \"#{strftime_nsec to}\", but got \"#{strftime_nsec random_time}\""
     end
   end
 
   def test_between_pseudorandom
-    from = ::Time.local(2012, 05, 05,  05,  05,  33, 222_222.222).round(9)
-    to   = ::Time.local(2012, 05, 05,  05,  05,  33, 222_222.224).round(9)
-    expected = ::Time.local(2012, 05, 05,  05,  05,  33, 222_222.223).round(9)
+    from = ::Time.local(2012, 05, 05,  05,  05,  33, 599_992.369).round(9)
+    to   = ::Time.local(2012, 05, 05,  05,  05,  33, 599_992.371).round(9)
+    expected = ::Time.local(2012, 05, 05,  05,  05,  33, 599_992.370).round(9)
 
     100.times do
       random_time = @tester.between(from, to)
-      assert random_time == expected, "Expected \"#{strftime_nsec expected}\", but got #{strftime_nsec random_time}"
+      assert random_time == expected, "Expected \"#{strftime_nsec expected}\", but got \"#{strftime_nsec random_time}\""
     end
   end
 
   def test_forward
     now = ::Time.now.round(9)
-    a_minute_later = now + 1*60
+    a_minute_later = (now + 1*60).round(9)
 
     100.times do
       random_time = @tester.forward(now, 60)
       assert random_time > now, "Expected > \"#{strftime_nsec now}\", but got #{strftime_nsec random_time}"
-      assert random_time < a_minute_later, "Expected < \"#{strftime_nsec a_minute_later}\", but got #{strftime_nsec random_time}"
+      assert random_time < a_minute_later, "Expected < \"#{strftime_nsec a_minute_later}\", but got \"#{strftime_nsec random_time}\""
+    end
+  end
+
+  def test_forward_pseudorandom
+    now = ::Time.now.round(9)
+    expected = (now + 0.000_000_001).round(9)
+
+    100.times do
+      random_time = @tester.forward(now, 0.000_000_002)
+      assert random_time == expected, "Expected > \"#{strftime_nsec expected}\", but got \"#{strftime_nsec random_time}\""
     end
   end
 
   def test_backward
     now = ::Time.now.round(9)
-    a_minute_earlier = now - 1*60
+    a_minute_earlier = (now - 1*60).round(9)
 
     100.times do
       random_time = @tester.backward(now, 60)
-      assert random_time < now, "Expected < \"#{strftime_nsec now}\", but got #{strftime_nsec random_time}"
-      assert random_time > a_minute_earlier, "Expected > \"#{strftime_nsec a_minute_earlier}\", but got #{strftime_nsec random_time}"
+      assert random_time < now, "Expected < \"#{strftime_nsec now}\", but got \"#{strftime_nsec random_time}\""
+      assert random_time > a_minute_earlier, "Expected > \"#{strftime_nsec a_minute_earlier}\", but got \"#{strftime_nsec random_time}\""
+    end
+  end
+
+    def test_backward_pseudorandom
+    now = ::Time.now.round(9)
+    expected = (now - 0.000_000_001).round(9)
+
+    100.times do
+      random_time = @tester.backward(now, 0.000_000_002)
+      assert random_time == expected, "Expected > \"#{strftime_nsec expected}\", but got \"#{strftime_nsec random_time}\""
     end
   end
 
@@ -56,7 +76,7 @@ class TestFactoryHelperTimestamps < Test::Unit::TestCase
     random_forward  = @tester.forward(now, 30)
 
     [random_backward, random_between, random_forward].each do |result|
-      assert result.is_a?(Time), "Expected a Time object, but got #{result.class}"
+      assert result.is_a?(Time), "Expected a Time object, but got \"#{result.class}\""
     end
   end
 
